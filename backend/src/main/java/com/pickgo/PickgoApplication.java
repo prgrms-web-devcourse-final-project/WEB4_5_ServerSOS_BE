2ff2e471104c6e92d26f1ab3cbec5b327ce4235d
min429 <lsm990429@gmail.com>
2025-04-28 01:34:36 +0900

    feat: BaseEntity 추가

--- backend/src/main/java/com/pickgo/PickgoApplication.java
+++ backend/src/main/java/com/pickgo/PickgoApplication.java
@@ -2,8 +2,10 @@ package com.pickgo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PickgoApplication {
 
 	public static void main(String[] args) {
