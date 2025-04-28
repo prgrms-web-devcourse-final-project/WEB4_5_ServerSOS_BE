f8b89f608a7eeeb44accbf5bbd430b57604e4934
jin214930 <jin214930@naver.com>
2025-04-28 20:46:29 +0900

    feat: repository 구현

---
+++ backend/src/main/java/com/pickgo/performance/area/repository/PerformanceAreaRepository.java
@@ -0,0 +1,7 @@
+package com.pickgo.performance.area.repository;
+
+import com.pickgo.performance.area.entity.PerformanceArea;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PerformanceAreaRepository extends JpaRepository<PerformanceArea, Long> {
+}
