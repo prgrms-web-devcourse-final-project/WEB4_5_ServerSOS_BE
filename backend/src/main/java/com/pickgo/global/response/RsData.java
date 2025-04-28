e0536cfcd1c24b170816c88102d23b0e7689df73
이승민 <101695929+min429@users.noreply.github.com>
2025-04-28 12:54:31 +0900

    chore: BaseEntity 필드 수정, RsData Json ignore 설정 (#4)

--- backend/src/main/java/com/pickgo/global/response/RsData.java
+++ backend/src/main/java/com/pickgo/global/response/RsData.java
@@ -1,8 +1,11 @@
 package com.pickgo.global.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Builder;
 
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record RsData<T>(
 	Integer code,
 	String message,
