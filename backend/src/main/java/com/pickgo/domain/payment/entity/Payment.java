0e45db0f21322e8cc218af3030aaaf056f2cca0b
JunKiBeom <dona0408@gmail.com>
2025-04-29 10:52:11 +0900

    feat: Payment Entity 구현

---
+++ backend/src/main/java/com/pickgo/domain/payment/entity/Payment.java
@@ -0,0 +1,28 @@
+package com.pickgo.domain.payment.entity;
+
+import com.pickgo.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@Builder
+public class Payment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status; // 결제 상태 (PENDING, COMPLETED, FAILED, CANCELLED)
+
+    @Column(nullable = false)
+    private Long amount;
+
+    @OneToOne
+    @JoinColumn(name = "reservation_id", unique = true, nullable = false)
+    private Reservation reservation;
+}
