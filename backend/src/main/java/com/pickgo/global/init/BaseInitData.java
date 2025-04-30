3ac1a6b1302a1e57b432ff7128d1a431c7962cfc
jin214930 <jin214930@naver.com>
2025-04-30 10:25:04 +0900

    comment: 주석 추가

--- backend/src/main/java/com/pickgo/global/init/BaseInitData.java
+++ backend/src/main/java/com/pickgo/global/init/BaseInitData.java
@@ -20,6 +20,7 @@ public class BaseInitData {
                 return;
             }
 
+            // 필요 시 주석 해제 후 사용 데이터 저장에 10분 가량 소요될 수 있음
 //            performanceService.fetchAndSavePerformances();
 //            System.out.println("초기 데이터 세팅이 완료되었습니다.");
         };
