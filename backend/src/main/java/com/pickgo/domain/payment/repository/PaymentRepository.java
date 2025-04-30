7925ea7f6fc9da733df54c4eca0a8fedb0ab1c14
JunKiBeom <dona0408@gmail.com>
2025-04-30 12:07:18 +0900

    feat: getMyPayments를 위한 findByReservationMember 메서드 추가

--- backend/src/main/java/com/pickgo/domain/payment/repository/PaymentRepository.java
+++ backend/src/main/java/com/pickgo/domain/payment/repository/PaymentRepository.java
@@ -1,7 +1,11 @@
 package com.pickgo.domain.payment.repository;
 
+import com.pickgo.domain.member.entity.Member;
 import com.pickgo.domain.payment.entity.Payment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Page<Payment> findByReservationMember(Member member, Pageable pageable);
 }
