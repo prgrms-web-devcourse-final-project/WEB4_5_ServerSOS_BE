8ebb3cf9298c963e43fcda82f28375fb0ce86f00
asungkim <130833844+asungkim@users.noreply.github.com>
2025-04-30 10:08:12 +0900

    배포 설정  (#13)
    * [chore]
    * application.yml 작성
    * docker-compose, Dockerfile 추가
    
    * [feat] 테라폼 코드 추가
    
    * [fix] yml 수정
    
    * [fix] application.yml 수정
    * prod에 중복된 datasoruce
    
    * [refactor] 누락된 부분 추가
    
    * [refactor] 테라폼 깃설치 추가
    
    * [refactor] 테라폼 코드 리펙토링
    * 내부적으로 docker login & 클론
    * env랑 docker-compose 만 하면됨
    
    * [feat] deploy, 테라폼 코드 테크 추가
    * deploy.yml 작성
    * 테라폼 prefix 수정
    * 테라폼 테그 부착

---
+++ infra/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  description = "Prefix for all resources"
+  default     = "team02"
+}
+
+variable "region" {
+  description = "region"
+  default     = "ap-northeast-2"
+}
+
+variable "nickname" {
+  description = "nickname"
+  default     = "asungkim"
+}
