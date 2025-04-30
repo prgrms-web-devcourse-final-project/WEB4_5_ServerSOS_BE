acf858da65f32c119db1c0ae8fe12d96e2297e16
JunKiBeom <dona0408@gmail.com>
2025-04-30 11:31:13 +0900

    chore: PaymentStatus 이름 변경

--- backend/src/main/java/com/pickgo/domain/payment/entity/Payment.java
+++ backend/src/main/java/com/pickgo/domain/payment/entity/Payment.java
@@ -1,5 +1,6 @@
 package com.pickgo.domain.payment.entity;
 
+import com.pickgo.domain.reservation.entity.Reservation;
 import com.pickgo.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -17,7 +18,7 @@ public class Payment extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private Status status; // 결제 상태 (PENDING, COMPLETED, FAILED, CANCELLED)
+    private PaymentStatus status; // 결제 상태 (PENDING, COMPLETED, FAILED, CANCELLED)
 
     @Column(nullable = false)
     private Long amount;
--- backend/src/main/java/com/pickgo/domain/payment/entity/PaymentStatus.java
+++ backend/src/main/java/com/pickgo/domain/payment/entity/PaymentStatus.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public enum Status {
+public enum PaymentStatus {
     PENDING("결제 대기"),
     COMPLETED("결제 완료"),
     FAILED("결제 실패"),
