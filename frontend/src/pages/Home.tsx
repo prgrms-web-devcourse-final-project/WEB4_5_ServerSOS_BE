3654df18a6958e0345e147b7178c9d80c8acc446
locke <hayeong28@naver.com>
2025-05-01 18:39:32 +0900

    feat: home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,14 @@
+import CategoryNavigation from "@/components/home/CategoryNavigation"
 import { PageLayout } from "../layout/PageLayout"
+import RankingBanner from "@/components/home/RankingBanner"
+import { Sections } from "@/components/home/Sections/Sections"
 
 export const Home = () => {
   return (
     <PageLayout>
-      <div>Home</div>
+      <CategoryNavigation />
+      <RankingBanner />
+      <Sections />
     </PageLayout>
   )
 }
