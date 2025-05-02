44d2bd222099acec783aba0e0a3b52addd1f56ba
ChoHyunWoo <gusdndlek12@naver.com>
2025-05-02 11:41:33 +0900

    refactor: TestCode 수정

--- backend/src/test/java/com/pickgo/admin/AdminPostControllerTest.java
+++ backend/src/test/java/com/pickgo/admin/AdminPostControllerTest.java
@@ -1,9 +1,11 @@
 package com.pickgo.admin;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.pickgo.domain.admin.dto.PostSimpleResponse;
 import com.pickgo.domain.admin.dto.PostUpdateRequest;
 import com.pickgo.domain.admin.service.AdminPostService;
+import com.pickgo.domain.performance.entity.Performance;
+import com.pickgo.domain.post.entity.Post;
+import com.pickgo.domain.venue.entity.Venue;
 import com.pickgo.global.response.RsCode;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
@@ -46,21 +51,44 @@ class AdminPostControllerTest {
     @Test
     @DisplayName("게시글 전체 목록 조회 성공")
     void getPostList() throws Exception {
-        when(adminPostService.getAllPosts(true)).thenReturn(
-                List.of(
-                        new PostSimpleResponse(1L, "게시글1", "공연장1", LocalDate.now(), LocalDate.now(), "poster1.jpg"),
-                        new PostSimpleResponse(2L, "게시글2", "공연장2", LocalDate.now(), LocalDate.now(), "poster2.jpg")
-                )
+        // given
+        List<Post> postEntities = List.of(
+                Post.builder().id(1L).title("게시글1")
+                        .performance(
+                                Performance.builder()
+                                        .poster("poster1.jpg")
+                                        .endDate(LocalDate.now())
+                                        .venue(Venue.builder().name("공연장1").build())
+                                        .build()
+                        ).build(),
+                Post.builder().id(2L).title("게시글2")
+                        .performance(
+                                Performance.builder()
+                                        .poster("poster2.jpg")
+                                        .endDate(LocalDate.now())
+                                        .venue(Venue.builder().name("공연장2").build())
+                                        .build()
+                        ).build()
         );
 
+        Page<Post> postPage = new PageImpl<>(postEntities, PageRequest.of(0, 10), postEntities.size());
+
+        // adminPostService는 Page<Post>를 반환하므로, 여기까지가 given
+        when(adminPostService.getAllPosts(1, 10, true)).thenReturn(postPage);
+
+        // when & then
         mockMvc.perform(get("/api/admin/posts")
-                .param("isPublished", "true"))
+                        .param("page", "1")
+                        .param("size", "10")
+                        .param("isPublished", "true"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.code").value(RsCode.SUCCESS.getCode()))
-                .andExpect(jsonPath("$.data[0].id").value(1L))
-                .andExpect(jsonPath("$.data[1].id").value(2L))
+                .andExpect(jsonPath("$.data.items[0].id").value(1L))
+                .andExpect(jsonPath("$.data.items[1].id").value(2L))
                 .andDo(print());
     }
+
+
 
     @Test
     @DisplayName("게시글 수정 성공")
