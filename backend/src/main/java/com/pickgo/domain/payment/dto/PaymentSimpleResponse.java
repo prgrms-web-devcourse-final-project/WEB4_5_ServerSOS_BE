2d47229741fdc7279ea5baeabde7dc5624b52fbf
JunKiBeom <dona0408@gmail.com>
2025-05-07 02:46:51 +0900

    feat: Response 필요한 데이터 리턴 하도록 수정

--- backend/src/main/java/com/pickgo/domain/payment/dto/PaymentSimpleResponse.java
+++ backend/src/main/java/com/pickgo/domain/payment/dto/PaymentSimpleResponse.java
@@ -6,12 +6,14 @@ import com.pickgo.domain.payment.entity.PaymentStatus;
 public record PaymentSimpleResponse(
         Long id,
         Integer amount,
+        String performanceName,
         PaymentStatus paymentStatus
 ) {
     public static PaymentSimpleResponse from(Payment payment) {
         return new PaymentSimpleResponse(
                 payment.getId(),
                 payment.getAmount(),
+                payment.getReservation().getPerformanceSession().getPerformance().getName(),
                 payment.getStatus()
         );
     }
