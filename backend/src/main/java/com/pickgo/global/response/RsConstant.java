321d960c9f8cd1ee96b023d93b7807f8d165bd04
ChoHyunWoo <gusdndlek12@naver.com>
2025-05-07 21:11:47 +0900

    refactor: AdminPostRepository 코드 수정

--- backend/src/main/java/com/pickgo/global/response/RsConstant.java
+++ backend/src/main/java/com/pickgo/global/response/RsConstant.java
@@ -39,15 +39,15 @@ public class RsConstant {
 	 */
 	public static Integer INTERNAL_SERVER_ERROR = HttpStatus.INTERNAL_SERVER_ERROR.value();
 
-	public static final int PERFORMANCE_NOT_FOUND = 4101;
+	public static  Integer PERFORMANCE_NOT_FOUND;
 
 	/*게시물 관련*/
-	public static final int POST_NOT_FOUND = 4201;
+	public static  Integer POST_NOT_FOUND;
 
 	/*리뷰 관련*/
-	public static final int REVIEW_CREATED = 4301;
-	public static final int REVIEW_DELETED = 4302;
-	public static final int REVIEW_UPDATED = 4303;
+	public static Integer REVIEW_CREATED;
+	public static Integer REVIEW_DELETED;
+	public static Integer REVIEW_UPDATED;
 
 	public static Integer CONFLICT = HttpStatus.CONFLICT.value();
 }
