6e6a1552bf878987867cdda4bf6db7c078d6d29f
JunKiBeom <dona0408@gmail.com>
2025-05-09 02:48:32 +0900

    feat: 게시글 API 인증 제외 경로 추가

--- backend/src/main/java/com/pickgo/global/jwt/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/pickgo/global/jwt/JwtAuthenticationFilter.java
@@ -1,7 +1,12 @@
 package com.pickgo.global.jwt;
 
-import java.io.IOException;
-
+import com.pickgo.global.exception.BusinessException;
+import com.pickgo.global.exception.jwt.JwtAuthenticationEntryPoint;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -9,14 +14,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.pickgo.global.exception.BusinessException;
-import com.pickgo.global.exception.jwt.JwtAuthenticationEntryPoint;
-
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import java.io.IOException;
 
 /**
  * 토큰 검증 및 사용자 인증 정보를 저장하는 필터
@@ -25,51 +23,52 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-	private final static String HEADER_AUTHORIZATION = "Authorization";
-	private final JwtProvider jwtProvider;
-	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final static String HEADER_AUTHORIZATION = "Authorization";
+    private final JwtProvider jwtProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
-	/**
-	 * 스프링 시큐리티 필터에서 인증 처리
-	 **/
-	@Override
-	protected void doFilterInternal(
-		HttpServletRequest request,
-		@NonNull HttpServletResponse response,
-		@NonNull FilterChain filterChain) throws ServletException, IOException {
+    /**
+     * 스프링 시큐리티 필터에서 인증 처리
+     **/
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
 
-		// 인증하지 않고 통과시킬 경로 설정
-		if (request.getRequestURI().equals("/api/tokens")
-			|| request.getRequestURI().equals("/api/members")
-			|| request.getRequestURI().equals("/api/members/login")
-			|| request.getRequestURI().startsWith("/api/examples")
-			|| request.getRequestURI().startsWith("/api/oauth")
-			|| request.getRequestURI().startsWith("/swagger-ui")
-			|| request.getRequestURI().startsWith("/v3/api-docs")
-		) {
-			filterChain.doFilter(request, response);
-			return;
-		}
+        // 인증하지 않고 통과시킬 경로 설정
+        if (request.getRequestURI().equals("/api/tokens")
+                || request.getRequestURI().equals("/api/members")
+                || request.getRequestURI().equals("/api/members/login")
+                || request.getRequestURI().startsWith("/api/examples")
+                || request.getRequestURI().startsWith("/api/oauth")
+                || request.getRequestURI().startsWith("/swagger-ui")
+                || request.getRequestURI().startsWith("/v3/api-docs")
+                || request.getRequestURI().startsWith("/api/posts")
+        ) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
-		String authorizationHeader = request.getHeader(HEADER_AUTHORIZATION);
-		String accessToken;
+        String authorizationHeader = request.getHeader(HEADER_AUTHORIZATION);
+        String accessToken;
 
-		try {
-			accessToken = jwtProvider.getAccessToken(authorizationHeader); // 헤더에서 액세스 토큰 가져옴
-			jwtProvider.validateToken(accessToken);
-		} catch (BusinessException e) {
-			jwtAuthenticationEntryPoint.commence(request, response, e);
-			return;
-		} catch (AuthenticationException e) {
-			jwtAuthenticationEntryPoint.commence(request, response, e);
-			return;
-		}
+        try {
+            accessToken = jwtProvider.getAccessToken(authorizationHeader); // 헤더에서 액세스 토큰 가져옴
+            jwtProvider.validateToken(accessToken);
+        } catch (BusinessException e) {
+            jwtAuthenticationEntryPoint.commence(request, response, e);
+            return;
+        } catch (AuthenticationException e) {
+            jwtAuthenticationEntryPoint.commence(request, response, e);
+            return;
+        }
 
-		// 사용자 인증 정보 - Principal(신원 ex. 아이디), Credentials(자격 증명 ex. 비밀번호), Authorities(권한)
-		Authentication authentication = jwtProvider.getAuthentication(accessToken);
-		// SecurityContext에 사용자 인증 정보 저장 -> 언제든 전역에서 접근 가능
-		SecurityContextHolder.getContext().setAuthentication(authentication);
+        // 사용자 인증 정보 - Principal(신원 ex. 아이디), Credentials(자격 증명 ex. 비밀번호), Authorities(권한)
+        Authentication authentication = jwtProvider.getAuthentication(accessToken);
+        // SecurityContext에 사용자 인증 정보 저장 -> 언제든 전역에서 접근 가능
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
-		filterChain.doFilter(request, response);
-	}
+        filterChain.doFilter(request, response);
+    }
 }
