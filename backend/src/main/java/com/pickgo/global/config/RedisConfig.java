aa7efe8fbc33411f536b0fd2709ec36abfa902d9
min429 <lsm990429@gmail.com>
2025-05-08 12:42:26 +0900

    chore: 환경변수 추가, redis 설정

---
+++ backend/src/main/java/com/pickgo/global/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.pickgo.global.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+	@Value("${spring.data.redis.host}")
+	private String host;
+
+	@Value("${spring.data.redis.port}")
+	private int port;
+
+	@Bean
+	public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+		return new StringRedisTemplate(connectionFactory);
+	}
+
+	@Bean
+	public RedissonClient redissonClient() {
+		Config config = new Config();
+		config.useSingleServer()
+			.setAddress("redis://" + host + ":" + port);
+		return Redisson.create(config);
+	}
+}
