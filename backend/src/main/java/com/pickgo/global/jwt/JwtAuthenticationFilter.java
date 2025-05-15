b51b546a77083830c5400aa59dd4872e56ffa3e6
이승민 <101695929+min429@users.noreply.github.com>
2025-05-15 14:39:40 +0900

    [refactor] jwtProvider 도메인으로부터 분리 (#108)
    * refactor: jwtProvider가 도메인에 의존하지 않도록 변경
    
    * refactor: 관련 테스트 코드 수정
    
    * refactor: 토큰 검증 시 예외를 던지지 않도록 하여 불필요한 try-catch 제거
    
    * refactor: 관련 테스트 코드 수정
    
    * refactor: 관련 테스트 코드 수정
    
    ---------
    
    Co-authored-by: asungkim <130833844+asungkim@users.noreply.github.com>

--- backend/src/main/java/com/pickgo/global/jwt/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/pickgo/global/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,12 @@
 package com.pickgo.global.jwt;
 
+import static com.pickgo.global.response.RsCode.*;
+import java.io.IOException;
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 import com.pickgo.global.exception.BusinessException;
 import com.pickgo.global.exception.jwt.JwtAuthenticationEntryPoint;
 import jakarta.servlet.FilterChain;
@@ -54,17 +61,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             return;
         }
 
+        // 토큰 검증
         String authorizationHeader = request.getHeader(HEADER_AUTHORIZATION);
-        String accessToken;
-
-        try {
-            accessToken = jwtProvider.getAccessToken(authorizationHeader); // 헤더에서 액세스 토큰 가져옴
-            jwtProvider.validateToken(accessToken);
-        } catch (BusinessException e) {
-            jwtAuthenticationEntryPoint.commence(request, response, e);
-            return;
-        } catch (AuthenticationException e) {
-            jwtAuthenticationEntryPoint.commence(request, response, e);
+        String accessToken = jwtProvider.getTokenFromHeader(authorizationHeader);
+        if (!jwtProvider.isValidToken(accessToken)) {
+            jwtAuthenticationEntryPoint.commence(request, response, new BusinessException(UNAUTHENTICATED));
             return;
         }
 
