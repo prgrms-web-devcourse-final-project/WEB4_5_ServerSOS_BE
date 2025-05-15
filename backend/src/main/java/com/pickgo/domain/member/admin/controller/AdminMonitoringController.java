b5aafa34b0c7b827337aa9465c8932165f453760
asungkim <asungkim007@naver.com>
2025-05-15 16:55:31 +0900

    [chore] 현우님 코드 추가 * docker-compose 수정 * health 체크 코드 추가

--- backend/src/main/java/com/pickgo/domain/member/admin/controller/AdminMonitoringController.java
+++ backend/src/main/java/com/pickgo/domain/member/admin/controller/AdminMonitoringController.java
@@ -3,6 +3,7 @@ package com.pickgo.domain.member.admin.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/admin/monitoring")
@@ -11,5 +12,11 @@ public class AdminMonitoringController {
     @GetMapping
     public String monitoringDashboard() {
         return "admin/monitoring";
+    }
+
+    @GetMapping("/health")
+    @ResponseBody
+    public String healthCheck() {
+        return "Ok";
     }
 }
