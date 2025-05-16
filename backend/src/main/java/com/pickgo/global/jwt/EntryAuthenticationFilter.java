6acd202937fdafeb52db89ec5b69a2365c6bd47f
min429 <lsm990429@gmail.com>
2025-05-16 11:59:23 +0900

    chore: 인증 관련 변경 - 입장 검증 방식 변경 (토큰만 검증) - 인증 관련 경로 수정

--- backend/src/main/java/com/pickgo/global/jwt/EntryAuthenticationFilter.java
+++ backend/src/main/java/com/pickgo/global/jwt/EntryAuthenticationFilter.java
@@ -1,21 +1,13 @@
 package com.pickgo.global.jwt;
 
-import static com.pickgo.domain.queue.enums.EntryState.*;
-import static com.pickgo.domain.queue.repository.redis.RedisEntryRepository.*;
 import static com.pickgo.global.response.RsCode.*;
 
 import java.io.IOException;
-import java.util.UUID;
 
 import org.springframework.lang.NonNull;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.pickgo.domain.member.member.dto.MemberPrincipal;
-import com.pickgo.domain.queue.enums.EntryState;
-import com.pickgo.domain.queue.repository.EntryRepository;
 import com.pickgo.global.exception.BusinessException;
 import com.pickgo.global.exception.jwt.JwtAuthenticationEntryPoint;
 
@@ -31,7 +23,6 @@ public class EntryAuthenticationFilter extends OncePerRequestFilter {
 
     private static final String HEADER_ENTRY_AUTH = "EntryAuth";
     private final JwtProvider jwtProvider;
-    private final EntryRepository entryRepository;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
     @Override
@@ -41,44 +32,24 @@ public class EntryAuthenticationFilter extends OncePerRequestFilter {
         @NonNull FilterChain filterChain) throws ServletException, IOException {
 
         // 인증할 경로 설정
-        if (!(request.getRequestURI().equals("/api/payments") && "POST".equals(request.getMethod())
-            || request.getRequestURI().equals("/api/payments/confirm")
-            || request.getRequestURI().equals("/api/areas") && "GET".equals(request.getMethod())
-            || request.getRequestURI().equals("/api/reservations") && "POST".equals(request.getMethod())
+        if (!(request.getRequestURI().equals("/api/areas") && "GET".equals(request.getMethod())
+                || request.getRequestURI().equals("/api/areas/subscribe")
+                || (request.getRequestURI().equals("/api/reservations") && "POST".equals(request.getMethod()))
         )) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        UUID userId = getUserIdFromSecurityContext();
-        EntryState state = entryRepository.getState(userId);
+        // 입장 토큰 가져오기
+        String entryAuthHeader = request.getHeader(HEADER_ENTRY_AUTH);
+        String entryToken = jwtProvider.getTokenFromHeader(entryAuthHeader);
 
-        // ACTIVE 상태인 경우 통과
-        if (state == ACTIVE) {
-            filterChain.doFilter(request, response);
+        // 토큰 검증
+        if (!jwtProvider.isValidToken(entryToken)) {
+            jwtAuthenticationEntryPoint.commence(request, response, new BusinessException(UNAUTHENTICATED));
             return;
         }
 
-        // 상태가 null이거나 PENDING인 경우 토큰 검증 후 ACTIVE로 전환
-        if (isValidToken(request, response)) {
-            entryRepository.setState(userId, ACTIVE, MAX_ACTIVE_TIMEOUT_MINUTES);
-            filterChain.doFilter(request, response);
-        }
-    }
-
-    private boolean isValidToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String entryAuthHeader = request.getHeader(HEADER_ENTRY_AUTH);
-        String entryToken = jwtProvider.getTokenFromHeader(entryAuthHeader);
-        if (!jwtProvider.isValidToken(entryToken)) {
-            jwtAuthenticationEntryPoint.commence(request, response, new BusinessException(UNAUTHENTICATED));
-            return false;
-        }
-        return true;
-    }
-
-    private UUID getUserIdFromSecurityContext() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        MemberPrincipal principal = (MemberPrincipal)authentication.getPrincipal();
-        return principal.id();
+        filterChain.doFilter(request, response);
     }
 }
