3078c29256e725a9ec12af0098f31ba4216559da
jin214930 <jin214930@naver.com>
2025-05-31 06:25:51 +0900

    fix: 클래식/무용 -> 클래식 수정

--- frontend/src/components/category/constants.ts
+++ frontend/src/components/category/constants.ts
@@ -6,7 +6,7 @@ export const CATEGORIES: { id: GetPopularPostsTypeEnum; name: string }[] = [
   { id: "MUSICAL", name: "뮤지컬" },
   { id: "CONCERT", name: "콘서트" },
   { id: "PLAY", name: "연극" },
-  { id: "CLASSIC", name: "클래식/무용" },
+  { id: "CLASSIC", name: "클래식" },
   { id: "DANCE", name: "무용" },
   { id: "KOREAN", name: "국악" },
   { id: "ETC", name: "기타" },
