08a90d7c7f3621bbdf2610c529ea0f2df5085db5
jin214930 <jin214930@naver.com>
2025-06-01 21:11:52 +0900

    fix: 조회수 기능 버그 수정

--- backend/src/main/java/com/pickgo/domain/post/post/util/IdentifierResolver.java
+++ backend/src/main/java/com/pickgo/domain/post/post/util/IdentifierResolver.java
@@ -1,16 +1,30 @@
 package com.pickgo.domain.post.post.util;
 
 import com.pickgo.domain.member.member.dto.MemberPrincipal;
+import com.pickgo.global.jwt.JwtProvider;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class IdentifierResolver {
-    public static String resolve(MemberPrincipal principal, HttpServletRequest request) {
-        if (principal != null) {
-            return "MEMBER_" + principal.id();
-        } else {
-            return "IP_" + request.getRemoteAddr();
+    private final JwtProvider jwtProvider;
+
+    public String resolve(HttpServletRequest request) {
+        try {
+            String authorizationHeader = request.getHeader("Authorization");
+            String accessToken = jwtProvider.getTokenFromHeader(authorizationHeader);
+            Authentication authentication = jwtProvider.getAuthentication(accessToken);
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof MemberPrincipal(java.util.UUID id)) {
+                return "MEMBER_" + id.toString();
+            }
+        } catch (Exception ignored) {
+            
         }
+
+        return "IP_" + request.getRemoteAddr();
     }
 }
